c00979ef6a2d6144824187d6c2b7ca0b308e35dc
yuler <is.yuler@gmail.com>
2024-06-13 13:14:55 +0800

    Invoke some api in home page

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,21 +1,18 @@
 import type { IApp } from '../app'
+import type { Quote } from '../types'
 import enhancePage from '../enhance-page'
 
 const $app = getApp<IApp>()
 
 enhancePage({
   data: {
-    time: new Date().toLocaleString(),
+    quote: null as Quote | null,
   },
-  onLoad() {
-    /**
-     * Event come from `app`
-     * @example events
-     */
-    $app.$on('app:tick', () => {
-      this.setData({
-        time: new Date().toLocaleString(),
-      })
+  async onLoad() {
+    const data = await $app.$api({
+      url: '/quotes/today',
+      method: 'GET',
     })
+    this.setData({ quote: data })
   },
 })
